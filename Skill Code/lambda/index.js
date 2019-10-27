ad38201203da04dda5683692796b6e822e9857fd
Goel Biju <goelmolel@hotmail.com>
2019-10-27 23:13:52 +0000

    Added interceptor and request handler for birthday (#6)

--- Skill Code/lambda/index.js
+++ Skill Code/lambda/index.js
@@ -17,6 +17,35 @@ const LaunchRequestHandler = {
             .getResponse();
     }
 };
+
+const HasBirthdayLaunchRequestHandler = {
+    canHandle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes() || {};
+
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest' && year && month && day;
+    },
+    handle(handlerInput) {
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        // TODO: Use the settings API to get current date and then compute how many days
+        //       until the user's birthday.
+        // TODO: Say Happy Birthday on the user's birthday.
+
+        const speakOutput = `Welcome back. It looks like there are X more days until your y-th birthday.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
 const CaptureBirthdayIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -117,6 +146,21 @@ const ErrorHandler = {
     }
 };
 
+const LoadBirthdayInterceptor = {
+    async process(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = await attributesManager.getPersistentAttributes() || {};
+
+        const year = sessionAttributes.hasOwnProperty('year') ? sessionAttributes.year : 0;
+        const month = sessionAttributes.hasOwnProperty('month') ? sessionAttributes.month : 0;
+        const day = sessionAttributes.hasOwnProperty('day') ? sessionAttributes.day : 0;
+
+        if (year && month && day) {
+            attributesManager.setSessionAttributes(sessionAttributes);
+        }
+    }
+};
+
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
@@ -125,12 +169,16 @@ exports.handler = Alexa.SkillBuilders.custom()
         new persistenceAdapter.S3PersistenceAdapter({bucketName:process.env.S3_PERSISTENCE_BUCKET})
     )
     .addRequestHandlers(
+        HasBirthdayLaunchRequestHandler,
         LaunchRequestHandler,
         CaptureBirthdayIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
         SessionEndedRequestHandler,
         IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    )
+    .addRequestInterceptors(
+        LoadBirthdayInterceptor
     )
     .addErrorHandlers(
         ErrorHandler,
